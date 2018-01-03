01081d0d6c215372e736ae81161b1336736ac4e9
sara-izaap <saravanan.izaap@gmail.com>
2018-01-03 12:32:26 +0530

    new changes

--- src/providers/config.ts
+++ src/providers/config.ts
@@ -1,2 +1,1 @@
-export let SERVER_URL = "http://162.144.41.156/~izaapinn/construction/index.php/service/";
-//"http://localhost/construction/service/"
+export let SERVER_URL = "http://www.davinci-construction.com/service/";
